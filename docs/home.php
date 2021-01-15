f81d4c4bfec5081cfc2c2a3e394ea6cc11599930
Fadhil Andriawan <58591531+Fadhil-Sabar@users.noreply.github.com>
2021-01-15 20:34:52 +0700

    Add files via upload

---
+++ docs/home.php
@@ -0,0 +1,141 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Tugas</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/tes.js"></script>
+    <!-- Custom styles for this template -->
+    <link href="assets/css/heroic-features.css" rel="stylesheet">  
+      <!-- Bootstrap core JavaScript -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+</head>
+<style>
+.card-img-top {
+    height:328px;
+}
+</style>
+<body>
+
+< <!-- Navigation 
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+-->
+  <!-- Page Content -->
+  <div class="container">
+
+    <!-- Jumbotron Header -->
+    <header class="jumbotron my-4 text-center">
+      <h1 class="display-3 text-center" >Selamat Datang</h1>
+      <p>Fadhil Andriawan XII RPL I</p>
+    </header>
+
+    <!-- Page Features -->
+    <div class="row text-center">
+    <div class="col-4 bg-success">.col-4</div>
+
+      <div class="col-lg-2 col-md-6 mb-4 ">
+        <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">Supplier</h4>
+          </div>
+          <img class="card-img-top" src="assets/images/supplier1.jpg" alt="">
+        </div>
+      </div>
+
+      <div class="col-lg-2 col-md-6 mb-4">
+        <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">Barang</h4>
+          </div>
+          <img class="card-img-top" src="assets/images/barang1.jpg" alt="">
+        </div>
+      </div>
+
+      <div class="col-lg-2 col-md-6 mb-4">
+        <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">Stok</h4>
+          </div>
+          <img class="card-img-top" src="assets/images/stok.jpg" alt="">
+        </div>
+      </div>
+    </div>
+    <!-- /.row -->
+        <!-- Page Features -->
+        <div class="row text-center">
+
+
+<div class="col-lg-4 col-md-6 mb-4">
+  <div class="card">
+  <div class="card-body">
+      <h4 class="card-title">Barang Masuk</h4>
+    </div>
+    <img class="card-img-top" src="assets/images/barang_masukk.jpg" alt="">
+  </div>
+</div>
+
+<div class="col-lg-4 col-md-6 mb-4">
+  <div class="card">
+  <div class="card-body">
+      <h4 class="card-title">Barang Keluar</h4>
+    </div>
+    <img class="card-img-top" src="assets/images/barang_keluar.jpg" alt="">
+  </div>
+</div>
+
+<div class="col-lg-4 col-md-6 mb-4">
+  <div class="card">
+  <div class="card-body">
+      <h4 class="card-title">Barang Pinjam</h4>
+    </div>
+    <img class="card-img-top" src="assets/images/pinjam_barang.jpg" alt="">
+  </div>
+</div>
+</div>
+
+  </div>
+  
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
